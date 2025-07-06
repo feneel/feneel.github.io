dc23b4134637f8b19af236a5dcf627e2603bbf0c
feneel <feneeldoshi@gmail.com>
2025-07-06 17:15:26 -0400

    Updating styling for About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,12 @@
 import { aboutText } from "../data/data";
+
 export default function About() {
   return (
     <section className="container py-5" data-aos="fade-up">
       <h2>About Me</h2>
-      <p style={{ whiteSpace: "pre-line" }}>{aboutText}</p>
+      <p style={{ whiteSpace: "pre-line", lineHeight: "1.8rem" }}>
+        {aboutText}
+      </p>
     </section>
   );
 }
