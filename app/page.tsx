314dedc3f90e6a507ba1cde266bf13537d3e68fd
feneel <feneeldoshi@gmail.com>
2025-07-06 17:16:33 -0400

    Adding in styling for Landing page and updating css modules

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,37 @@
-'use client'
-import { Typewriter } from 'react-simple-typewriter';
+"use client";
+import { Typewriter } from "react-simple-typewriter";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className="container text-center py-5" data-aos="fade-up">
-      <h1 className="display-4">
-        Hi, I’m Feneel 👋<br />
-        <span style={{ color: '#007bff' }}>
-          <Typewriter
-            words={[
-              
-              'Full-Stack Developer',
-              'MERN Stack Enthusiast',
-              'Always learning'
-              
-            ]}
-            loop={0}
-            cursor
-            cursorStyle="_"
-            typeSpeed={70}
-            deleteSpeed={50}
-            delaySpeed={1500}
-          />
-        </span>
-      </h1>
-      <p className="lead">I love building scalable apps and solving meaningful problems.</p>
-      <a href="/projects" className="btn btn-primary mt-3">View My Work</a>
-    </div>
+    <section className={styles.hero} data-aos="fade-up">
+      <div className={styles.textWrapper}>
+        <h1 className={styles.heading}>
+          Hi, I’m Feneel 👋
+          <br />
+          <span className={styles.typewriter}>
+            <Typewriter
+              words={[
+                "Full-Stack Developer",
+                "MERN Stack Enthusiast",
+                "Always learning",
+              ]}
+              loop={0}
+              cursor
+              cursorStyle="_"
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1500}
+            />
+          </span>
+        </h1>
+        <p className={styles.subtext}>
+          I love building scalable apps and solving meaningful problems.
+        </p>
+        <a href="/projects" className={styles.ctaButton}>
+          View My Work
+        </a>
+      </div>
+    </section>
   );
 }
