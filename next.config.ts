434d69f85890264a750ef7d9712b06484288279d
feneel <feneeldoshi@gmail.com>
2025-07-06 22:17:58 -0400

    Deployment: Adding distDir in config

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /**
-   * Enable static exports.
-   *
-   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-   */
+const nextConfig = {
   output: "export",
-
-  /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: "/feneel.github.io",
-
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
+  assetPrefix: "./",
   images: {
     unoptimized: true,
   },
+  distDir: 'out'
+
 };
 
-export default nextConfig;
+module.exports = nextConfig;
